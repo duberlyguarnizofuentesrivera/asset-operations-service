0950452d49de60b462087a5b9670ac0470d4687d
Duberly, Guarnizo Fuentes Rivera <duberlygfr@gmail.com>
2023-09-21 13:44:53 -0500

    Initial Commit

---
+++ src/test/java/com/duberlyguarnizo/assetoperationsservice/AssetOperationsServiceApplicationTests.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2023. Code by Duberly Guarnizo <duberlygfr@gmail.com>.
+ */
+
+package com.duberlyguarnizo.assetoperationsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetOperationsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
